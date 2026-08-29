41252f6c68a0cd8e3a4e07aaad5d1d79d4ebf087
cbcc9hvz8w-create <cbcc9hvz8w@privaterelay.appleid.com>
2026-08-29 14:28:41 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "v3.4.18"; // build:version
+const APP_VERSION = "v3.4.19"; // build:version
 // Keep this cache namespace separate from the preserved Anki Sprint v9.13 PWA.
 const CACHE_PREFIX = "apkg-simulator-shell-";
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
@@ -6,8 +6,8 @@ const PRECACHE = [
   "./.nojekyll",
   "./assets/apple-touch-icon-C3GtdD9x.png",
   "./assets/icon-gTp1paoi.svg",
-  "./assets/index-BMn7zG2z.js",
   "./assets/index-Dd4FS1F1.css",
+  "./assets/index-SeEauKqg.js",
   "./assets/manifest-DD1pHFoS.webmanifest",
   "./assets/sql-wasm-UFUCzYNW.wasm",
   "./icons/apple-touch-icon.png",
